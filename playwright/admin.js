ca664c4b53f2e38c19088465f97a0f03332ddf51
iwakami <iwagame@gmail.com>
2021-08-08 22:23:51 +0900

    upd

--- playwright/admin.js
+++ playwright/admin.js
@@ -1,12 +1,19 @@
 const playwright = require('playwright');
 
-(async () => {
+/**
+ * 
+ * @param {string} vid 
+ * @param {string} time 
+ */
+const capture = async (vid, time) => {
    const browserType = 'chromium';
-   const browser = await playwright[browserType].launch({headless: true, devtools: false});
-   const context = await browser.newContext({viewport: {width:720, height:720}});
+   const browser = await playwright[browserType].launch({headless: false, devtools: true});
+   const context = await browser.newContext({viewport: {width:720, height:720}, locale: 'en-US'});
    const page = await context.newPage(); 
 
-   await page.goto('https://www.youtube-nocookie.com/embed/kJdp-hJxrAk?start=' + (20*60+18));
+   const min = Number(time.split(":")[0]);
+   const sec = Number(time.split(":")[1]);
+   await page.goto('https://www.youtube-nocookie.com/embed/'+ vid+ '?start=' + (min*60+sec));
 
     
    await page.click('[aria-label="Play"]');
@@ -15,9 +22,12 @@ const playwright = require('playwright');
    await page.waitForSelector(':not(.unstarted-mode).playing-mode', {timeout:1000});
 
    const el = await page.$('video');
-   await el.screenshot({path: 'screen.png'});
+   await el.screenshot({path: 'screen' + '.png'});
+   console.log(await page.title());
+   await browser.close();
+};
 
-   return browser.close();
-})();
+const url = new URL("https://www.youtube.com/watch?v=kJdp-hJxrAk")
+capture(url.searchParams.get("v"), "20:18");
 
 console.log('done');
