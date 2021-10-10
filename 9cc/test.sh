cf82db08ede698561f479fd5781e81eeddae1bc8
iwakami <iwagame@gmail.com>
2021-10-10 19:40:14 +0900

    Makefile and test.sh

---
+++ 9cc/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+assert() {
+    expected="$1"
+    input="$2"
+
+    rm tmp.s
+    go run 9cc.go "$input" > tmp.s
+    cc -o tmp tmp.s
+    ./tmp
+    actual="$?"
+
+    if [ "$actual" = "$expected" ]; then
+        echo "$input => $actual"
+    else
+        echo "$input => $expected expected but got $actual"
+        exit 1
+    fi
+}
+
+assert 0 0
+assert 42 42
+assert 255 255
+
+echo OK
